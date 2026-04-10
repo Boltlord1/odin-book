f34e4f6aabda871ba746955c31790bbe98fe35fc
Boltlord1 <boltlord1@gmail.com>
2026-04-10 08:02:14 +0500

    Initial commit

---
+++ src/main.tsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import routes from './routes.tsx'
+
+const root = document.getElementById('root')
+
+if (root !== null) {
+	const router = createBrowserRouter(routes)
+	createRoot(root).render(
+		<StrictMode>
+			<RouterProvider router={router} />
+		</StrictMode>
+	)
+}
